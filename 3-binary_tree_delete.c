2616273b21cca27b60be3a51644f5128e492b90b
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-03-26 20:05:42 +0200

    replace

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 #include "binary_trees.h"
-
+/**
+ * postorderTraversal - traferese and free node
+ * @root: current node
+ * Return: void
+ */
 void postorderTraversal(binary_tree_t *root)
 {
 	if (root == NULL)
@@ -9,7 +13,11 @@ void postorderTraversal(binary_tree_t *root)
 	postorderTraversal(root->right);
 	free(root);
 }
-
+/**
+ * binary_tree_delete - free binary tree
+ * @tree: tree root
+ * Return: void
+ */
 void binary_tree_delete(binary_tree_t *tree)
 {
 	postorderTraversal(tree);
