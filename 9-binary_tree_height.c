135bf879980416ffabf7e59b2fc39dee8f36445a
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-03-26 22:29:39 +0200

    9. Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,7 +11,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	int lDepth = 0, rDepth = 0;
 
 	if (tree == NULL)
-		return 0;
+		return (0);
 
 	if (tree->left)
 		lDepth = 1 + binary_tree_height(tree->left);
