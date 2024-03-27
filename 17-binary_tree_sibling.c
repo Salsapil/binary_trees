640b1e74e551c5126bc1e778d4c24b49ddc4a662
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-03-27 14:48:58 +0200

    17. Sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,15 +1,19 @@
 #include <stdlib.h>
 #include "binary_trees.h"
-
+/**
+ * binary_tree_sibling - find sibiling
+ * @node: node to get sibling
+ * Return: binary_tree_t or null
+ */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (node == NULL || node->parent == NULL)
-        return (NULL);
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
 
-    if (node->parent->left && node->n == node->parent->left->n)
-    {
-        return (node->parent->right);
-    }
-    else
-        return (node->parent->left);
+	if (node->parent->left && node->n == node->parent->left->n)
+	{
+		return (node->parent->right);
+	}
+	else
+		return (node->parent->left);
 }
