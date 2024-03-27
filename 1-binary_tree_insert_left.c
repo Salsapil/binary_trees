9bb87953946ffc584176226617dfcd2b51821c11
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-03-27 16:26:50 +0200

    1. Insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,10 +9,12 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *bt = malloc(sizeof(binary_tree_t));
+	binary_tree_t *bt;
 
 	if (!parent)
 		return (NULL);
+
+	bt = malloc(sizeof(binary_tree_t));
 	if (!bt)
 		return (NULL);
 	bt->n = value;
